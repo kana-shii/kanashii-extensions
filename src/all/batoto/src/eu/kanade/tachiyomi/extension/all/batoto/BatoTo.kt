a1056895952f9d321b5379dd3a33dccf995ca3f4
kana-shii <79055104+kana-shii@users.noreply.github.com>
2025-04-01 20:36:48 -0300

    Update BatoTo.kt

--- src/all/batoto/src/eu/kanade/tachiyomi/extension/all/batoto/BatoTo.kt
+++ src/all/batoto/src/eu/kanade/tachiyomi/extension/all/batoto/BatoTo.kt
@@ -92,10 +92,16 @@ open class BatoTo(
             summary = preferences.getString("${REMOVE_TITLE_CUSTOM_PREF}_$lang", "") ?: ""
             setDefaultValue("")
         }
+        val openLinksInV3xPref = CheckBoxPreference(screen.context).apply {
+            key = "${OPEN_LINKS_IN_V3X_PREF_KEY}_$lang"
+            title = "Open links in v3x"
+            setDefaultValue(true)
+        }
         screen.addPreference(mirrorPref)
         screen.addPreference(altChapterListPref)
         screen.addPreference(removeOfficialPref)
         screen.addPreference(removeCustomPref)
+        screen.addPreference(openLinksInV3xPref)
     }
 
     private var mirror = ""
@@ -129,6 +135,8 @@ open class BatoTo(
     }
     private fun customRemoveTitle(): String =
         preferences.getString("${REMOVE_TITLE_CUSTOM_PREF}_$lang", "") ?: ""
+
+    private fun getOpenLinksInV3xPref(): Boolean = preferences.getBoolean("${OPEN_LINKS_IN_V3X_PREF_KEY}_$lang", false)
 
     private fun SharedPreferences.migrateMirrorPref() {
         val selectedMirror = getString("${MIRROR_PREF_KEY}_$lang", MIRROR_PREF_DEFAULT_VALUE)!!
@@ -1043,6 +1051,29 @@ open class BatoTo(
         CheckboxFilterOption("pt-PT", "Portuguese (Portugal)"),
     ).filterNot { it.value == siteLang }
 
+    private val v3xMangaUrlRegex = Regex("(.+)/series/([0-9]+)/.+")
+    private val v3xChapterUrlRegex = Regex("(.+)/chapter/([0-9]+)")
+
+    override fun getMangaUrl(manga: SManga): String {
+        if (getOpenLinksInV3xPref()) {
+            val (_, baseUrl, mangaId) =
+                v3xMangaUrlRegex.matchEntire(super.getMangaUrl(manga))?.groupValues
+                    ?: return super.getMangaUrl(manga)
+            return "$baseUrl/title/$mangaId"
+        }
+        return super.getMangaUrl(manga)
+    }
+
+    override fun getChapterUrl(chapter: SChapter): String {
+        if (getOpenLinksInV3xPref()) {
+            val (_, baseUrl, chapterId) =
+                v3xChapterUrlRegex.matchEntire(super.getChapterUrl(chapter))?.groupValues
+                    ?: return super.getChapterUrl(chapter)
+            return "$baseUrl/title/0/$chapterId"
+        }
+        return super.getChapterUrl(chapter)
+    }
+
     companion object {
         private const val MIRROR_PREF_KEY = "MIRROR"
         private const val MIRROR_PREF_TITLE = "Mirror"
@@ -1089,5 +1120,6 @@ open class BatoTo(
         private const val ALT_CHAPTER_LIST_PREF_TITLE = "Alternative Chapter List"
         private const val ALT_CHAPTER_LIST_PREF_SUMMARY = "If checked, uses an alternate chapter list"
         private const val ALT_CHAPTER_LIST_PREF_DEFAULT_VALUE = false
+        private const val OPEN_LINKS_IN_V3X_PREF_KEY = "OPEN_LINKS_IN_V3X"
     }
 }
